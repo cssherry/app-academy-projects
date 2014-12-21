1bba0c89abb478b579588622be55eafce0938ef1
zhoux10 <5643f56c@opayq.com>
2014-12-20 18:57:39 -0800

    Finished dealer hand

--- 01Ruby/W2_a02-practice/lib/dealer.rb
+++ 01Ruby/W2_a02-practice/lib/dealer.rb
@@ -10,14 +10,24 @@ class Dealer < Player
   end
 
   def place_bet(dealer, amt)
+    raise "Dealer doesn't bet"
   end
 
   def play_hand(deck)
+    until hand.points >= 17
+      hand.hit(deck)
+    end
   end
 
   def take_bet(player, amt)
+    @bets[player] = amt
   end
 
   def pay_bets
+    winners = @bets.select { |player| player.hand.beats?(hand) }
+    winners.each do |player, value|
+      player.pay_winnings(value * 2) # This is a strange way to pay out people... technically should be paying out total pot divided among each winner right?
+    end
+    @bets = {}
   end
 end
