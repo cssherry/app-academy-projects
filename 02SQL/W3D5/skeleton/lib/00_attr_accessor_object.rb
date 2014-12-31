250fe1e5fb2fb9cd08bd58fe08ba873e72e99794
zhoux10 <5643f56c@opayq.com>
2014-12-31 14:29:57 -0800

    Finish first exercise

--- 02SQL/W3D5/skeleton/lib/00_attr_accessor_object.rb
+++ 02SQL/W3D5/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,12 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+
+    names.each do |name|
+      instance_variable = "@#{name}"
+      define_method("#{name}") { instance_variable }
+
+      define_method("#{name}=") { |value| instance_variable = value }
+    end
+
   end
 end
