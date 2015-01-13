f55cfdb9ebb1e4d146365e161b87a3b13881be12
Sherry Zhou <5643f56c@opayq.com>
2015-01-13 15:02:43 -0800

    Finish phase 4

--- 03Rails/W5D2/rails_lite/lib/phase4/controller_base.rb
+++ 03Rails/W5D2/rails_lite/lib/phase4/controller_base.rb
@@ -4,13 +4,18 @@ require_relative './session'
 module Phase4
   class ControllerBase < Phase3::ControllerBase
     def redirect_to(url)
+      super
+      @session.store_session(@res)
     end
 
     def render_content(content, type)
+      super
+      @session.store_session(@res)
     end
 
     # method exposing a `Session` object
     def session
+      @session ||= Session.new(@req)
     end
   end
 end
--- 03Rails/W5D2/rails_lite/lib/phase4/session.rb
+++ 03Rails/W5D2/rails_lite/lib/phase4/session.rb
@@ -6,17 +6,27 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @cookies = cookies_for_app(req)
+      @cookies ||= {}
+    end
+
+    def cookies_for_app(req)
+      cookie = req.cookies.find { |cookie| cookie.name == '_rails_lite_app'}
+      JSON.parse(cookie.value) unless cookie.nil?
     end
 
     def [](key)
+      @cookies[key]
     end
 
     def []=(key, val)
+      @cookies[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new('_rails_lite_app', @cookies.to_json)
     end
   end
 end
