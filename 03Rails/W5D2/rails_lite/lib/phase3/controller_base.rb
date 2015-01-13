d4ef518da917b30cf5252d394b759067e0b9175b
Sherry Zhou <5643f56c@opayq.com>
2015-01-13 14:31:08 -0800

    Finish phase 2

--- 03Rails/W5D2/rails_lite/lib/phase3/controller_base.rb
+++ 03Rails/W5D2/rails_lite/lib/phase3/controller_base.rb
@@ -7,6 +7,10 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      file = File.read("views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
+      template = ERB.new(file)
+      result = template.result(binding)
+      render_content(result, 'text/html')
     end
   end
 end
